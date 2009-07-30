2a548fd6433d4533e74294c396189f92e77510a1
ZsJoska <zsjoska@gmail.com>
2009-07-30 20:05:00 +0300

    added the gagarin-wsclient dependency to the gagarin-adminui

--- gagarin-adminui/src/main/scala/ro/gagarin/snippet/Login.scala
+++ gagarin-adminui/src/main/scala/ro/gagarin/snippet/Login.scala
@@ -8,6 +8,7 @@ import _root_.net.liftweb.http.S._
 import _root_.net.liftweb.http.SHtml._
 import _root_.net.liftweb.util.Helpers._
 import _root_.net.liftweb.util._
+import _root_.ro.gagarin.wsclient.WSClient
 
 class Login {
   def login (in: NodeSeq): NodeSeq  = {
@@ -17,7 +18,8 @@ class Login {
          "username" -> text("", (x)=> (u=x)),
          "password" -> password("", (x) =>(p=x)),
          "submit" -> submit("Login", () => {
-           notice("Logged in " + u + "("+p+")")
+           val session = WSClient.getWSClient("http://localhost:8080/ws/").getAuthentication().createSession(null, null)
+           notice("Logged in " + u + "("+p+")" + " session=" + session)
            redirectTo("/") })
     )
   } 

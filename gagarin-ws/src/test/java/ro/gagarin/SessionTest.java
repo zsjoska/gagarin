3146bb6f5ba67883553b6c419c23a495d8fb072d
unknown <zsjoska@.(none)>
2009-08-19 01:55:32 +0300

    modified the test to wait less for DB import

--- gagarin-ws/src/test/java/ro/gagarin/SessionTest.java
+++ gagarin-ws/src/test/java/ro/gagarin/SessionTest.java
@@ -7,10 +7,13 @@ import static org.junit.Assert.fail;
 import java.util.List;
 
 import org.apache.log4j.Logger;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import ro.gagarin.application.objects.AppUser;
 import ro.gagarin.config.Config;
+import ro.gagarin.config.DBConfigManager;
 import ro.gagarin.exceptions.DataConstraintException;
 import ro.gagarin.exceptions.ItemNotFoundException;
 import ro.gagarin.exceptions.OperationException;
@@ -25,9 +28,13 @@ import ro.gagarin.ws.Authentication;
  * Unit test for simple App.
  */
 public class SessionTest {
-	private static final transient Logger LOG = Logger.getLogger(SessionTest.class);
+	private static final transient Logger LOG = Logger
+			.getLogger(SessionTest.class);
 
-	private static final ManagerFactory FACTORY = BasicManagerFactory.getInstance();
+	private static final ManagerFactory FACTORY = BasicManagerFactory
+			.getInstance();
+
+	private static final long LOCAL_DB_CONFIG_CHECK_PERIOD = 100;
 
 	private Authentication authentication = new Authentication();
 	private String username = "_User_" + System.currentTimeMillis();
@@ -36,9 +43,23 @@ public class SessionTest {
 
 	// TODO: add test with null session for all WS methods
 
+	@BeforeClass
+	public static void startup() {
+		DBConfigManager.getInstance().configChanged(
+				Config.DB_CONFIG_CHECK_PERIOD,
+				"" + LOCAL_DB_CONFIG_CHECK_PERIOD);
+	}
+
+	@AfterClass
+	public static void shutdown() {
+		DBConfigManager dbConfig = DBConfigManager.getInstance();
+		dbConfig.configChanged(Config.DB_CONFIG_CHECK_PERIOD, ""
+				+ dbConfig.getLong(Config.DB_CONFIG_CHECK_PERIOD));
+	}
+
 	@Test
-	public void testSuccessLogin() throws SessionNotFoundException, DataConstraintException,
-			ItemNotFoundException, OperationException {
+	public void testSuccessLogin() throws SessionNotFoundException,
+			DataConstraintException, ItemNotFoundException, OperationException {
 
 		session = TUtil.createTestSession();
 
@@ -56,7 +77,8 @@ public class SessionTest {
 		AppUser user = new AppUser();
 		user.setUsername("1" + username);
 		user.setPassword("password1");
-		user.setRole(roleManager.getRoleByName(cfgManager.getString(Config.ADMIN_ROLE_NAME)));
+		user.setRole(roleManager.getRoleByName(cfgManager
+				.getString(Config.ADMIN_ROLE_NAME)));
 		userManager.createUser(user);
 		FACTORY.releaseSession(session);
 
@@ -69,8 +91,8 @@ public class SessionTest {
 	}
 
 	@Test
-	public void testFailedLogin() throws SessionNotFoundException, ItemNotFoundException,
-			DataConstraintException, OperationException {
+	public void testFailedLogin() throws SessionNotFoundException,
+			ItemNotFoundException, DataConstraintException, OperationException {
 
 		session = TUtil.createTestSession();
 
@@ -81,7 +103,8 @@ public class SessionTest {
 		DBUser user = new DBUser();
 		user.setUsername("2" + username);
 		user.setPassword("password2");
-		user.setRole(roleManager.getRoleByName(cfgManager.getString(Config.ADMIN_ROLE_NAME)));
+		user.setRole(roleManager.getRoleByName(cfgManager
+				.getString(Config.ADMIN_ROLE_NAME)));
 		userManager.createUser(user);
 
 		FACTORY.releaseSession(session);
@@ -104,8 +127,8 @@ public class SessionTest {
 	}
 
 	@Test
-	public void testSessionDeletion() throws ItemNotFoundException, DataConstraintException,
-			OperationException {
+	public void testSessionDeletion() throws ItemNotFoundException,
+			DataConstraintException, OperationException {
 		session = TUtil.createTestSession();
 
 		UserDAO userManager = FACTORY.getDAOManager().getUserDAO(session);
@@ -115,7 +138,8 @@ public class SessionTest {
 		DBUser user = new DBUser();
 		user.setUsername("3" + username);
 		user.setPassword("password3");
-		user.setRole(roleManager.getRoleByName(cfgManager.getString(Config.ADMIN_ROLE_NAME)));
+		user.setRole(roleManager.getRoleByName(cfgManager
+				.getString(Config.ADMIN_ROLE_NAME)));
 		userManager.createUser(user);
 
 		FACTORY.releaseSession(session);
@@ -128,30 +152,33 @@ public class SessionTest {
 			authentication.login(session, "3" + username, "password3", null);
 			fail("The login must fail since the session was deleted");
 		} catch (SessionNotFoundException e) {
-			assertEquals("Wrong session ID returned by the exception", e.getSessionID(), session);
+			assertEquals("Wrong session ID returned by the exception", e
+					.getSessionID(), session);
 		}
 
 	}
 
 	@Test
-	public void testSessionExpiration() throws InterruptedException, SessionNotFoundException,
-			OperationException {
+	public void testSessionExpiration() throws InterruptedException,
+			SessionNotFoundException, OperationException {
 
-		session = FACTORY.getSessionManager().createSession(null, null, FACTORY);
+		session = FACTORY.getSessionManager()
+				.createSession(null, null, FACTORY);
 		FACTORY.getSessionManager().acquireSession(session.getSessionString());
 
 		ConfigurationManager cfgManager = FACTORY.getConfigurationManager();
 		cfgManager.setConfigValue(session, Config.USER_SESSION_TIMEOUT, "100");
 		FACTORY.releaseSession(session);
 
-		long toWait = cfgManager.getLong(Config.DB_CONFIG_CHECK_PERIOD);
-		LOG.info("Waiting DB config import for " + toWait);
-		Thread.sleep(toWait);
+		LOG.info("Waiting DB config import for " + LOCAL_DB_CONFIG_CHECK_PERIOD);
+		Thread.sleep(LOCAL_DB_CONFIG_CHECK_PERIOD);
 
 		SessionManager sessionManager = FACTORY.getSessionManager();
-		Session session = FACTORY.getSessionManager().createSession(null, null, FACTORY);
+		Session session = FACTORY.getSessionManager().createSession(null, null,
+				FACTORY);
 		assertNotNull(session);
-		assertEquals("We just set the timeout to 100", session.getSessionTimeout(), 100);
+		assertEquals("We just set the timeout to 100", session
+				.getSessionTimeout(), 100);
 
 		try {
 			session = sessionManager.acquireSession(session.getSessionString());
